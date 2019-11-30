5f7cb7149299ed8d3387d97c542db5ac7ade9721
BirjuVachhani <brvachhani@gmail.com>
2019-11-30 12:58:42 +0530

    feat: enable databinding feat: modify base activity to support databinding

--- app/src/main/java/com/example/loginmvvm/base/BaseActivity.kt
+++ app/src/main/java/com/example/loginmvvm/base/BaseActivity.kt
@@ -3,15 +3,27 @@ package com.example.loginmvvm.base
 import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.example.loginmvvm.BR
 
-abstract class BaseActivity<VM : ViewModel>(@LayoutRes private val layoutID: Int) :
+abstract class BaseActivity<VM : BaseVM, VDB : ViewDataBinding>(@LayoutRes private val layoutID: Int) :
     AppCompatActivity() {
 
     abstract val viewModel: VM
+    protected lateinit var binding: VDB
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layoutID)
+        binding = DataBindingUtil.setContentView(this, layoutID)
+        binding.lifecycleOwner = this
+        binding.setVariable(BR.viewModel, viewModel)
     }
 }
+
+
+
+
+
+
+
