3e671c18edc6991da2b334bfc6bf543699632357
BirjuVachhani <brvachhani@gmail.com>
2019-11-30 13:11:34 +0530

    feat: add input validations

--- app/src/main/java/com/example/loginmvvm/login/MainActivity.kt
+++ app/src/main/java/com/example/loginmvvm/login/MainActivity.kt
@@ -3,10 +3,11 @@ package com.example.loginmvvm.login
 import android.os.Bundle
 import com.example.loginmvvm.R
 import com.example.loginmvvm.base.BaseActivity
+import com.example.loginmvvm.databinding.MainActivityBinding
 import com.example.loginmvvm.login.viewmodel.MainActivityVM
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class MainActivity : BaseActivity<MainActivityVM>(R.layout.activity_main) {
+class MainActivity : BaseActivity<MainActivityVM, MainActivityBinding>(R.layout.activity_main) {
 
     override val viewModel: MainActivityVM by viewModel()
 
--- app/src/main/java/com/example/loginmvvm/login/viewmodel/MainActivityVM.kt
+++ app/src/main/java/com/example/loginmvvm/login/viewmodel/MainActivityVM.kt
@@ -1,6 +1,30 @@
 package com.example.loginmvvm.login.viewmodel
 
+import android.util.Patterns
+import androidx.lifecycle.MutableLiveData
 import com.example.loginmvvm.base.BaseVM
 
 class MainActivityVM : BaseVM() {
+
+    val emailLiveData = MutableLiveData<String>("")
+    val passwordLiveData = MutableLiveData<String>("")
+
+    val pattern = Patterns.EMAIL_ADDRESS
+
+    fun login() {
+        val email = emailLiveData.value!!
+        val password = passwordLiveData.value!!
+
+        // validation
+        if (email.isBlank() || !pattern.matcher(email).matches()) {
+            // invalid email
+            return
+        }
+        if (password.isBlank() || password.length !in 8..20) {
+            // invalid password
+            return
+        }
+
+        // proceed for login
+    }
 }
